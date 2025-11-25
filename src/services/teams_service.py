d15cc2b7f62cfd1674446d4584c2ea3c6ad692c5
= <nguyenkhaan2006@gmail.com>
2025-11-25 13:48:33 +0700

    them_kt_role

--- src/services/teams_service.py
+++ src/services/teams_service.py
@@ -4,6 +4,28 @@ from ..models.user_model import User
 from ..models.task_model import Task 
 from sqlalchemy.orm import aliased 
 from ..models.association import team_member_association, assignment_association
+from sqlalchemy import select 
+
+def isMember(user_id, team_id):
+    stmt = select(team_member_association).where(team_member_association.c.user_id == user_id, team_member_association.c.team_id == team_id)
+    
+    result = db.session.execute(stmt).first
+
+    if(result): return True
+    return False
+
+def isViceLeader(user_id, team_id):
+    team = Team.query.get(team_id)
+    vice_leader_id = team.get('vice_leader_id')
+    if(vice_leader_id == user_id): return True
+    return False
+
+def isLeader(user_id, team_id):
+    team = Team.query.get(team_id)
+    leader_id = team.get('leader_id')
+    if(leader_id == user_id): return True
+    return False
+
 def getTeams(): 
     datas = db.session.query(Team).all() 
     datas = [data.to_dict() for data in datas] 
