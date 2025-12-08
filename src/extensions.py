d41afc3cda8a47490deeab6bad4f5e6eb3cadc66
nguyenkhaan <nguyenkhaan2006@gmail.com>
2025-12-08 23:28:05 +0700

    chinh_redis

--- src/extensions.py
+++ src/extensions.py
@@ -4,8 +4,8 @@ import os
 
 load_dotenv()
 
-app_cache = Redis(host='localhost', port=6379, password=os.getenv('REDIS_PASSWORD'), decode_responses=True, db=0)
+app_cache = Redis(host=os.environ.get('REDISHOST'), port=6379, password=os.getenv('REDIS_PASSWORD'), decode_responses=True, db=0)
 
-jwt_blacklist = Redis(host='localhost', port=6379, password=os.getenv('REDIS_PASSWORD'), decode_responses=True, db=0) 
+jwt_blacklist = Redis(host=os.environ.get('REDISHOST'), port=6379, password=os.getenv('REDIS_PASSWORD'), decode_responses=True, db=0) 
 
 
