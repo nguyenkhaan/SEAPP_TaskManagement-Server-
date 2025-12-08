36483e77045725613ef627f235d7b625239d4bcf
Andexenn <phanhuynhhung0707@gmail.com>
2025-12-08 07:50:12 +0700

    remove cache

--- src/services/datetime_service.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timedelta 
-def getNow(): 
-    return datetime.now() 
-def addTime(datetime , hour = 0 , minute = 0 , day = 0 , second = 0):
-    delta = timedelta(hours=hour , minutes=minute , days=day , seconds=second)
-    return datetime + delta 
-def toStr(datetime): 
-    return str(datetime) 
--- src/utils.py
+++
@@ -1,5 +0,0 @@
-from cloudinary.utils import cloudinary_url
-
-def getImageUrl(public_id):
-    result_tuple = cloudinary_url(public_id, secure=True)
-    return result_tuple[0]
