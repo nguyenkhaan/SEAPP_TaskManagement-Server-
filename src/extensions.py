56c3000f8658947a079b4ab08d27ff0d65df1e71
nguyenkhaan <nguyenkhaan2006@gmail.com>
2025-12-08 23:57:21 +0700

    chinh_redis

--- src/extensions.py
+++ src/extensions.py
@@ -1,8 +1,17 @@
 import os 
 from redis import Redis
 
-# Giả sử bạn đã set REDIS_URL trong environment:
-# REDIS_URL = "redis://:mypassword@redis-production-363b.up.railway.app:6379/0"
-
-app_cache = Redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
-jwt_blacklist = Redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
+app_cache = Redis(
+    host="127.0.0.1",
+    port=6379,
+    password=os.environ.get('REDISPASSWORD'),
+    decode_responses=True,
+    db=0
+)
+jwt_blacklist = Redis(
+    host="127.0.0.1",
+    port=6379,
+    password=os.environ.get('REDISPASSWORD'),
+    decode_responses=True,
+    db=0
+)
