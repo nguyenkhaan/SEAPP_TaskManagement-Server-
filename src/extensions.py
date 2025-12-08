4be8c5745bb6f611c549b4b8dc50183da328bd4d
nguyenkhaan <nguyenkhaan2006@gmail.com>
2025-12-09 00:06:56 +0700

    chinh_redis

--- src/extensions.py
+++ src/extensions.py
@@ -1,17 +1,6 @@
 import os 
 from redis import Redis
 
-app_cache = Redis(
-    host="127.0.0.1",
-    port=6379,
-    password=os.environ.get('REDISPASSWORD'),
-    decode_responses=True,
-    db=0
-)
-jwt_blacklist = Redis(
-    host="127.0.0.1",
-    port=6379,
-    password=os.environ.get('REDISPASSWORD'),
-    decode_responses=True,
-    db=0
-)
+app_cache = Redis.from_url(os.environ["REDIS_URL"], decode_responses=True)
+jwt_blacklist = Redis.from_url(os.environ["REDIS_URL"], decode_responses=True)
+
